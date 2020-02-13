ccfee7f14db20eefd9d67bc49da3cc35a5a7a456
Evrgreen <rcarsten06@gmail.com>
2020-02-13 09:35:32 -0700

    Final commit for turn in

--- src/index.js
+++ src/index.js
@@ -5,19 +5,17 @@ import { Router } from "@reach/router";
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 import Events from "./components/Events";
-import { useDarkMode } from "./hooks/useDarkMode";
+
 import {useAxios} from "./hooks/useAxios"
 
 import "./styles.scss";
 const App = () => {
-  const [darkMode, setDarkMode] = useDarkMode();
   const [coinData] = useAxios("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true")
  
 
 
   return (
-    <div className={darkMode ? "App dark-mode" : "App"}>
-      <button onClick={setDarkMode}>Toggle DarkMode</button>
+    <div className="App">
       <Navbar />
       <Router>
         <Events path="/events" />
